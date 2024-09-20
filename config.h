45e62a6490825c73b7e797b59bb128331d808b29
Maxwell Odri <maxwellodri@gmail.com>
2024-09-20 11:19:05 +1000

    add togglerice

--- config.h
+++ config.h
@@ -1,16 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static       unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gapsborderpx = 2;
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static       int warp_enabled = 1;
 static const char *fonts[] = { "FiraCode Nerd Font Mono", "Noto Color Emoji", "Twemoji" }; //fonts are ttf-joypixels, ttf-twemoji, and nerd-fonts-fira-code
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -18,16 +20,18 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_magenta[]        = "#ff5555";
+static const char col_red[]        = "#ff0000";
+static const char col_purplepink[]        = "#4c004c";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+    [SchemeSel]  = { col_gray4, col_purplepink,  col_red  },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static int warp_enabled = 1;
 void view_click(const Arg* arg) { //disable warp on clicks
     warp_enabled = 0;
     view(arg);
@@ -36,6 +40,7 @@ void view_click(const Arg* arg) { //disable warp on clicks
 void nowarpkillclient(const Arg* arg) {
     warp_enabled = 0;
     killclient(arg);
+    //set warp_enabled = 1; in dwm.c:unmanage
 }
 
 static const char *tagsel[][2] = {
@@ -61,12 +66,9 @@ static const Rule rules[] = {
     { "nvide_daemon", NULL, NULL,           0,          0,          1,          -1,        0,         -1,           -1 },
 	{ "St",      NULL,      NULL,           0,          0,          1,          0,         0,         -1,           -1 },
 	{ NULL,      NULL,      "Event Tester", 0,          0,          0,          -1,        0,         -1,           -1 }, /* xev */
-	{ "game",    NULL,      "bevy",		    1 << 0,     0,          0,          1,         0,         -1,           +0  }, /* bevy game */
-	{ "bevy",    NULL,      "bevy",		    1 << 0,     0,          0,          1,         0,         -1,           +0  }, /* bevy game */
-	{ "bevy",    NULL,      "app",		    1 << 0,     0,          0,          1,         0,         -1,           +0 }, /* bevy game */
 	{ "discord", NULL,	    "Discord",      1 << 2,     0,          0,          0,         0,         -1,           +1  }, 
-	{ "Steam",   NULL,      "Steam",		1 << 1,     0,          0,          1,         0,         -1,           +1  }, /* steam */
-	{ "Signal",  NULL,      NULL,           1 << 0,     0,          0,          0,         0,         -1,           +1  }, 
+	{ "Steam",   NULL,      "Steam",		1 << 1,     0,          0,          1,         1,         -1,           +1  },
+	{ "Signal",  NULL,      NULL,           1 << 0,     0,          0,          0,         1,         -1,           +1  }, 
 };
 
 /* layout(s) */
@@ -79,6 +81,18 @@ static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen win
 #include "vanitygaps.c"
 #include "attach.c"
 #define PERTAG_PATCH
+
+void togglerice(const Arg *arg) {
+    borderpx=gapsborderpx-borderpx;
+    Monitor *m;
+    Client *c;
+    for (m = mons; m; m = m->next) {
+        for (c = m->clients; c; c = c->next) {
+            c->bw = borderpx;
+        }
+    }
+    togglegaps(arg);
+}
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -147,8 +161,8 @@ static const Key keys[] = {
 	//{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
 	//{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
 	//{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY,                       XK_semicolon,      togglerice,     {0} },
+	//{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	//{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_x,      nowarpkillclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, //tile
