9014639fd69371e3ee3fd5c9deaf176f955137d6
Maxwell Odri <maxwellodri@gmail.com>
2024-09-21 13:07:49 +1000

    adjust gaps

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 static       unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gapsborderpx = 2;
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 32;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 32;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
