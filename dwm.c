505b836300b8febec1cf88160b43640cfc6db7f7
Maxwell Odri <maxwellodri@gmail.com>
2024-09-29 06:45:32 +1000

    fix drawbar/floating icon and fullscreen client interaction

--- dwm.c
+++ dwm.c
@@ -984,7 +984,7 @@ drawbar(Monitor *m)
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
-			if (m->sel->isfloating)
+			if (m->sel->isfloating  && !m->sel->isfullscreen)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
@@ -1286,7 +1286,7 @@ killclient(const Arg *arg)
 {
 	if(!selmon->sel || selmon->sel->ispermanent > 0)
 		return;
-	if (!sendevent(selmon->sel, wmatom[WMDelete])) {
+	//if (!sendevent(selmon->sel, wmatom[WMDelete])) {
 		XGrabServer(dpy);
 		XSetErrorHandler(xerrordummy);
 		XSetCloseDownMode(dpy, DestroyAll);
@@ -1294,7 +1294,7 @@ killclient(const Arg *arg)
 		XSync(dpy, False);
 		XSetErrorHandler(xerror);
 		XUngrabServer(dpy);
-	}
+	//}
 }
 
 void
